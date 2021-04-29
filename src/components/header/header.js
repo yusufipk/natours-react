e9e3aae2364f9807fa2810e021423abb65167d13
Yusuf Ipek <yusufipek@protonmail.com>
2021-04-29 14:37:37 +0300

    added logo and title

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,20 @@
 import "./header.css";
+import logoWhite from "../../commons/images/logo-white.png";
 
 function Header() {
-  return <header className="header">Text...</header>;
+  return (
+    <header className="header">
+      <div className="logo-box">
+        <img src={logoWhite} alt="Logo" className="logo"></img>
+      </div>
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">Outdoors</span>
+          <span className="heading-primary-sub">is where life happens</span>
+        </h1>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
