9199a562e878b08c0e5c09b07cc07eb8951e140e
Yusuf Ipek <yusufipek@protonmail.com>
2021-05-01 15:49:54 +0300

    created text component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
+import About from "./commons/about-section/about";
 import Header from "./commons/header/header";
 
 function App() {
   return (
     <div>
       <Header />
+      <About />
     </div>
   );
 }
---
+++ src/commons/about-section/about.jsx
@@ -0,0 +1,19 @@
+function About() {
+  return (
+    <main>
+      <section className="section-about">
+        <div className="u-center-text u-margin-bottom-8">
+          <h2 className="heading-secondary">
+            Exciting tours for adventurous people
+          </h2>
+        </div>
+
+        <div className="row">
+          <div className="col-1-of-2">Text content</div>
+          <div className="col-1-of-2">Image</div>
+        </div>
+      </section>
+    </main>
+  );
+}
+export default About;
