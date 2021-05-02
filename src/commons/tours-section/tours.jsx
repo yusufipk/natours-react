11ea48a038330a9ff72d8f56f2ec89721445fb67
Yusuf Ipek <yusufipek@protonmail.com>
2021-05-02 14:21:03 +0300

    styled front side of the card component

--- src/commons/tours-section/tours.jsx
+++ src/commons/tours-section/tours.jsx
@@ -8,7 +8,23 @@ function Tours() {
       <div className="row">
         <div className="col-1-of-3">
           <div className="card">
-            <div className="card__side card__side--front">Front</div>
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture--1">&nbsp;</div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span--1">
+                  The Sea Explorer
+                </span>
+              </h4>
+              <div className="card__details">
+                <ul>
+                  <li>3 day tours</li>
+                  <li>Up to 30 people</li>
+                  <li>2 tour guides</li>
+                  <li>Sleep in cozy hotels</li>
+                  <li>Difficulty: easy</li>
+                </ul>
+              </div>
+            </div>
             <div className="card__side  card__side--back card__side--back-1">
               Back
             </div>
