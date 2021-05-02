12ab9e618c4fd61a2c382e123c7c89ea0ac3df47
Yusuf Ipek <yusufipek@protonmail.com>
2021-05-02 12:55:38 +0300

    created feature-box component and section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import About from "./commons/about-section/about";
-import Header from "./commons/header/header";
+import Feature from "./commons/feature-section/feature";
+import Header from "./commons/header/header.jsx";
 
 function App() {
   return (
     <div>
       <Header />
       <About />
+      <Feature />
     </div>
   );
 }
---
+++ src/commons/feature-section/feature.jsx
@@ -0,0 +1,58 @@
+function Feature() {
+  return (
+    <section className="section-features">
+      <div className="row">
+        <div className="col-1-of-4">
+          <div className="feature-box">
+            <i className="feature-box__icon fa fa-globe" />
+            <h3 className="heading-tertiary u-margin-bottom-small">
+              Explore the world
+            </h3>
+            <p className="feature-box__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+              iure ipsum facere quia quis dolorum laboriosam omnis esse.
+            </p>
+          </div>
+        </div>
+        <div className="col-1-of-4">
+          <div className="feature-box">
+            <i className="feature-box__icon fa fa-compass" />
+            <h3 className="heading-tertiary u-margin-bottom-small">
+              Meet nature
+            </h3>
+            <p className="feature-box__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+              iure ipsum facere quia quis dolorum laboriosam omnis esse.
+            </p>
+          </div>
+        </div>
+        <div className="col-1-of-4">
+          <div className="feature-box">
+            <i className="feature-box__icon fa fa-map" />
+            <h3 className="heading-tertiary u-margin-bottom-small">
+              Find your way
+            </h3>
+            <p className="feature-box__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+              iure ipsum facere quia quis dolorum laboriosam omnis esse.
+            </p>
+          </div>
+        </div>
+        <div className="col-1-of-4">
+          <div className="feature-box">
+            <i className="feature-box__icon fa fa-heart" />
+            <h3 className="heading-tertiary u-margin-bottom-small">
+              Live a healthier life
+            </h3>
+            <p className="feature-box__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+              iure ipsum facere quia quis dolorum laboriosam omnis esse.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Feature;
