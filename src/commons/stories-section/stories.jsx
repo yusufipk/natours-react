43bb82cfc10c2a980d87e6df96d1c9eaa65617ca
Yusuf Ipek <yusufipek@protonmail.com>
2021-05-03 13:39:30 +0300

    created bg-video component and finished stories section

--- src/commons/stories-section/stories.jsx
+++ src/commons/stories-section/stories.jsx
@@ -1,10 +1,19 @@
 import ButtonSecondary from "../../components/button/button-secondary";
 import nat8 from "../images/nat-8.jpg";
 import nat9 from "../images/nat-9.jpg";
+import bgmp4 from "../images/video.mp4";
+import bgwebm from "../images/video.webm";
 
 function Stories() {
   return (
     <section className="section-stories">
+      <div className="bg-video">
+        <video className="bg-video__content" autoPlay muted loop>
+          <source src={bgmp4} type="video/mp4" />
+          <source src={bgwebm} type="video/webm" />
+          Your browser is not supporting the video format!
+        </video>
+      </div>
       <div className="u-center-text u-margin-bottom-big">
         <h2 className="heading-secondary">We make people geniunely happy</h2>
       </div>
